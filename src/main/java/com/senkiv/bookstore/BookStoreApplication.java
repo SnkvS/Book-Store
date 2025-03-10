5ef46ed76a429fee6ef5b80e9f98ad51c61c19c5
Serhii Senkiv <senkiv34@gmail.com>
2025-03-10 20:59:53 +0400

    Test submission to db.

--- src/main/java/com/senkiv/bookstore/BookStoreApplication.java
+++ src/main/java/com/senkiv/bookstore/BookStoreApplication.java
@@ -1,5 +1,8 @@
 package com.senkiv.bookstore;
 
+import com.senkiv.bookstore.model.Book;
+import com.senkiv.bookstore.service.BookService;
+import java.math.BigDecimal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +10,20 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
     @Bean
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(BookService bookService) {
         return (String... args) -> {
-            System.out.println("Everything working fine)");
+            var book = new Book();
+            book.setTitle("Game of Thrones");
+            book.setAuthor("George Martin");
+            book.setIsbn("some_isbn");
+            book.setPrice(BigDecimal.ONE);
+            bookService.save(book);
+            System.out.println(bookService.findAll());
         };
     }
 
