5cd23c92d30f793196b6d7aba7842a40137c364e
Serhii Senkiv <senkiv34@gmail.com>
2025-03-10 08:44:43 +0400

    Initial commit -> 1. Project setup. 2. Infrastructure setup.

---
+++ src/test/java/com/senkiv/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senkiv.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
